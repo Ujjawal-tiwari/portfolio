df65140a4576c5af0d6469a978c2d13b5e76fc57
ujjawal tiwari <100299016+Ujjawal-tiwari@users.noreply.github.com>
2025-01-24 14:45:26 +0530

    Remove unused images and PDF files; update portfolio data with new project descriptions and achievements. Enhanced community and experience sections in HTML templates for improved user interaction and visual appeal.

--- data/portfolio_data.py
+++ data/portfolio_data.py
@@ -44,10 +44,40 @@ portfolio_data = {
     
     'projects': [
         {
-            'title': 'Doctor Prediction Model',
+            'title': 'AI Medical Consultation Model',
             'description': 'Developed a machine learning model to predict medical diagnoses based on patient symptoms and historical data. Implemented using advanced ML algorithms and achieved high accuracy in preliminary testing.',
             'technologies': ['Python', 'TensorFlow', 'Scikit-learn'],
+            'github_link': 'https://github.com/Ujjawal-tiwari/AI_assistant'
+        },
+         {
+            'title': 'Master system Integration',
+            'description': 'Master System Interation (MSI) uses AI/ML and WebSocket to create communication between software application and building infrastructure and healthcare applications by using different channels (bluetooth,wifi,usb) to reduce data lag and error and reducing human resources exchanging data in real time with the database application. The prime use of MSI is to regulate automated devices using mobile apps and web apps for example, a notification for fire alarm on your personal device or a remote heart rate or breath notification of patient`s to the nurse staff or a filler alert message to laundry or cleaning services from the smart dust bags. monitoring car parking on arrival of staff or visitors. automating ducts ,air regulators or steams before arrival of tenants.',
+            'technologies': ['Python', 'TensorFlow', 'Scikit-learn','FLask','WebSockets'],
+            'github_link': 'https://github.com/Ujjawal-tiwari/MSI'
+        },
+         {
+            'title': 'AI Legal Consultation Model',
+            'description': 'An AI chatbot that can answer any legal questions and provide legal advice as well as previous similer cases filed and verdicts of SC and HC. It uses the OpenAI API to generate responses and the Flask framework to create a web interface.',
+            'technologies': ['Python', 'OpenAI-API', 'Scikit-learn'],
             'github_link': 'https://github.com/yourusername/project1'
+        },
+         {
+            'title': 'object,image & text detection Model',
+            'description': 'An AI featured camera model that can detect objects, images and text in real time and provide a detailed report of the detected objects and their locations. It uses the Pytessaract library to detect text and the OpenCV library to detect images and objects.',
+            'technologies': ['Python', 'Pytessaract', 'Scikit-learn'],
+            'github_link': 'https://github.com/Ujjawal-tiwari/objectDetection'
+        },
+         {
+            'title': 'GPS tracking system,Asset management system',
+            'description': 'An AI featured camera model that can detect objects, images and text in real time and provide a detailed report of the detected objects and their locations. It uses the Pytessaract library to detect text and the OpenCV library to detect images and objects.',
+            'technologies': ['Python', 'Dijkstra Algorithm', 'Google Maps API'],
+            'github_link': 'https://github.com/Ujjawal-tiwari/GPS_tracker'
+        },
+         {
+            'title': 'IPTV cabling',
+            'description': 'IPTV cabling is a system that allows you to watch TV programmed by the user.it is highly used in hospitals,schools,offices and other places where agenda based TV broadcasting is required.',
+            'technologies': ['Python'],
+            'github_link': 'https://github.com/Ujjawal-tiwari/IPTV_cabling'
         },
         # Add more projects
     ],
@@ -64,19 +94,32 @@ portfolio_data = {
                 'Developed ML model with 95% accuracy',
                 'Reduced processing time by 30%'
             ],
-            'technologies': ['Python', 'TensorFlow', 'Flask','IoT']
+            'technologies': ['Python', 'TensorFlow', 'Flask', 'IoT']
         },
         {
             'company': 'Johnson Controls',
             'role': 'Web Developer',
+            'company_url': 'https://www.johnsoncontrols.com/',
             'duration': 'Jan 2023 - Oct 2023',
-            'description': 'Developed and maintained web applications using modern technologies. Implemented responsive designs and integrated backend services.'
+            'description': 'Developed and maintained web applications using modern technologies. Implemented responsive designs and integrated backend services.',
+            'logo': 'johnson.png',
+            'achievements': [
+                'Developed Web Application for the 3 tier fire detection alarming system',
+                'Reduced processing time by 60%'
+            ],
+            'technologies': ['Python', 'Flutter', 'Flask', 'IoT']
         },
         {
             'company': 'HighRaius Corporations',
             'role': 'Intern',
             'duration': 'Jan 2022 - May 2022',
-            'description': 'Gained hands-on experience in software development and AI/ML projects. Contributed to team projects and learned industry best practices.'
+            'company_url': 'https://www.highradius.com/',
+            'description': 'Gained hands-on experience in software development and AI/ML projects. Contributed to team projects and learned industry best practices.',
+            'logo': 'highradius.png',
+            'achievements': [
+                'Developed ML model with 95% accuracy',
+            ],
+            'technologies': ['Python', 'KNN Model', 'CNN Model']
         }
     ],
     
@@ -117,22 +160,72 @@ portfolio_data = {
             'degree': 'MBA in Information Technology',
             'institution': 'Uttaranchal University',
             'duration': '2024 - 2026',
-            'gpa': '8.8'
+            'gpa': '8.8',
+            'website': 'https://www.uudoon.in/',
+            'logo': 'UU.png',
+            'banner_image': 'johnson.png',
+                # Add more achievements
+            'semesters': [
+                {
+                    'name': 'Semester 1',
+                    'cgpa': '8.8',
+                },
+                # Add more semesters
+            ],
+
+            
         },
     ],
     
     'certifications': [
         {
-            'name': 'Problem Solving (Basic)',
+            'name': 'Node.js (Intermediate) Certificate',
             'issuer': 'HackerRank',
-            'date': '2023',
+            'date': '2022',
             'link': 'https://www.hackerrank.com/certificates/def2dac01412'
         },
         {
-            'name': 'Machine Learning Specialization',
+            'name': 'Problem Solving (Basic) Certificate',
+            'issuer': 'HackerRank',
+            'date': '2022',
+            'link': 'https://www.hackerrank.com/certificates/3975a529aa1e'
+        },
+         {
+            'name': 'Problem Solving (Intermediate) Certificate',
+            'issuer': 'HackerRank',
+            'date': '2022',
+            'link': 'https://www.hackerrank.com/certificates/289c60caa6d2'
+        },
+         {
+            'name': 'Node.js (Intermediate) Certificate',
+            'issuer': 'HackerRank',
+            'date': '2022',
+            'link': 'https://www.hackerrank.com/certificates/3975a529aa1e'
+        },
+        {
+            'name': 'Modernize Infrastructure and Applications with Google Cloud',
+            'issuer': 'Coursera',
+            'date': '2021',
+            'link': 'https://coursera.org/share/4f5b071e5bb5a96a6309bed0388319ab'
+        },
+        {
+            'name': 'The Role of Global Capital Markets',
             'issuer': 'Coursera',
             'date': '2023',
-            'link': 'https://coursera.org/share/4f5b071e5bb5a96a6309bed0388319ab'
+            'link': 'https://www.coursera.org/account/accomplishments/verify/9NGHLJ333E8Z?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course'
+
+        },
+        {
+            'name': ' Lessons and Applications for Investors',
+            'issuer': 'Coursera',
+            'date': '2021',
+            'link': 'https://www.coursera.org/account/accomplishments/verify/TCS5N84PRFUM?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course'
+        },
+        {
+            'name': 'Business Transformation with Google Cloud',
+            'issuer': 'Coursera',
+            'date': '2022',
+            'link': 'https://www.coursera.org/account/accomplishments/verify/D6XULDZ99MUJ?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course'
         },
         # Add more certifications as needed
     ],
@@ -140,19 +233,43 @@ portfolio_data = {
     'communities': [
         {
             'name': 'KIIT Robotics Society',
-            'role': 'Member',
+            'role': 'Associate Member',
             'duration': '2020 - Present',
             'description': 'Active member contributing to robotics projects and workshops.',
-            'image': 'krs_logo.png',  # Add community logo to static/images/communities/
+            'image': 'kii_robotics.jpg',  # Add community logo to static/images/communities/
             'link': 'https://kiit.ac.in/robotics/'
         },
         {
-            'name': 'Google Developer Student Clubs',
+            'name': ' AI Automation Agency Hub',
             'role': 'Member',
-            'duration': '2021 - Present',
-            'description': 'Participating in tech workshops and collaborative projects.',
-            'image': 'gdsc_logo.png',
-            'link': 'https://gdsc.community.dev/'
+            'duration': '2025 - Present',
+            'description': 'Participating in AI solution provider using promts and collaborative projects.',
+            'image': 'AAA.jpg',
+            'link': 'https://www.skool.com/learn-ai'
+        },
+        {
+            'name': 'KIIT Model United Nations',
+            'role': 'Member',
+            'duration': '2019 - 2022',
+            'description': 'Participating in premier global simulation of the United Nations.',
+            'image': 'MUN.jpg',
+            'link': 'https://kiitmun.org/'
+        },
+         {
+            'name': 'Wordsmith',
+            'role': 'Editor',
+            'duration': '2019 - Present',
+            'description': 'Senior content writer & Editor for the magazine.',
+            'image': 'wordsmith.jpg',
+            'link': 'https://ksac.kiit.ac.in/societies/kiit-wordsmith/'
+        },
+        {
+            'name': 'Kritika & Kirti Magazine',
+            'role': 'Senior Editor',
+            'duration': '2019 - Present',
+            'description': 'Senior content writer & Editor for the magazine.',
+            'image': 'kritika.jpg',
+            'link': 'https://ksac.kiit.ac.in/wp-content/uploads/2020/10/Kritika-2020.pdf'
         },
         # Add more communities as needed
     ],
